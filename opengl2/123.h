e9feafff6f435ed6fd3be8461725868c4036ac02
cloudytosunny <1287145695@qq.com>
2021-10-01 18:09:13 +0800

    add opengl2

---
+++ opengl2/123.h
@@ -0,0 +1,4 @@
+#ifndef 123_H
+#define 123_H
+
+#endif // 123_H
---
+++ opengl2/helloopengl.cpp
@@ -0,0 +1,134 @@
+﻿#include "helloopengl.h"
+#include "ui_helloopengl.h"
+
+//static Shader ourShader("../opengl1/shader/rect.vert","../opengl1/shader/rect.frag");
+
+
+HelloOpenGL::HelloOpenGL(QWidget *parent) :
+    QOpenGLWidget(parent),
+    ui(new Ui::HelloOpenGL)
+{
+    //ui->setupUi(this);
+
+
+}
+
+HelloOpenGL::~HelloOpenGL()
+{
+    delete ui;
+}
+
+void HelloOpenGL::initializeGL()
+{
+
+    core = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
+
+    GLfloat vertices[] = {
+          0.5f,  0.5f, 0.0f, 1.0f, 1.0f,  //右上
+          0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  //右下
+         -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  //左下
+         -0.5f,  0.5f, 0.0f, 0.0f, 1.0f   //左上
+    };
+
+    GLuint indices[] = {
+        0,1,3,
+        1,3,2
+    };
+
+
+    core->glGenVertexArrays(1, &VAO);
+    core->glBindVertexArray(VAO);
+
+
+    core->glGenBuffers(1,&VBO);
+    core->glGenBuffers(1,&EBO);
+
+    core->glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    core->glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
+    core->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof (GLfloat), (void*)0);
+    core->glEnableVertexAttribArray(0);
+    core->glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof (GLfloat), (void*)(3 * sizeof(GLfloat)));
+    core->glEnableVertexAttribArray(1);
+
+    core->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    core->glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices, GL_STATIC_DRAW);
+
+    core->glBindVertexArray(0);
+
+    core->glBindBuffer(GL_ARRAY_BUFFER,0);
+
+    core->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+    texture1 = new QOpenGLTexture(QImage("../opengl2/image/wall.jpg").mirrored(),QOpenGLTexture::GenerateMipMaps);
+    texture1->setWrapMode(QOpenGLTexture::Repeat);
+    texture1->setWrapMode(QOpenGLTexture::DirectionS,QOpenGLTexture::Repeat);
+    texture1->setWrapMode(QOpenGLTexture::DirectionT,QOpenGLTexture::Repeat);
+    texture1->setMinificationFilter(QOpenGLTexture::Nearest);
+    texture1->setMagnificationFilter(QOpenGLTexture::Linear);
+
+    texture2 = new QOpenGLTexture(QImage("../opengl2/image/face.bmp").mirrored(),QOpenGLTexture::GenerateMipMaps);
+    texture2->setWrapMode(QOpenGLTexture::Repeat);
+    texture2->setWrapMode(QOpenGLTexture::DirectionS,QOpenGLTexture::Repeat);
+    texture2->setWrapMode(QOpenGLTexture::DirectionT,QOpenGLTexture::Repeat);
+    texture2->setMinificationFilter(QOpenGLTexture::Nearest);
+    texture2->setMagnificationFilter(QOpenGLTexture::Linear);
+
+
+    ourShader = new Shader(QString("../opengl2/shader/rect.vert"),QString("../opengl2/shader/rect.frag"));
+    ourShader->use();
+    ourShader->shaderProgram.setUniformValue(ourShader->shaderProgram.uniformLocation("ourTexture"),0);
+    ourShader->shaderProgram.setUniformValue(ourShader->shaderProgram.uniformLocation("ourTexture2"),1);
+
+
+}
+void HelloOpenGL::paintGL()
+{
+    if(key == "Key_P"){core->glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);}
+    if(key == "Key_O"){core->glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);}
+
+    core->glClearColor(0.2f,0.3f,0.3f,1.0f);
+    core->glClear(GL_COLOR_BUFFER_BIT);
+
+    core->glActiveTexture(GL_TEXTURE0);
+    texture1->bind();
+    core->glActiveTexture(GL_TEXTURE1);
+    texture2->bind();
+
+    //shaderProgram.bind();
+    ourShader->use();
+    core->glBindVertexArray(VAO);
+    //core->glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    QMatrix4x4 mat;
+    //mat.scale(2);
+    //mat.translate(0.3f,0.1f,0.0f);
+    //mat.rotate(20.0f,QVector3D(0.0f,0.0f,1.0f));
+    mat.rotate(QTime::currentTime().second(),QVector3D(0.0f,0.0f,1.0f));
+    ourShader->setMat4("transform",mat);
+
+
+
+
+    core->glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+void HelloOpenGL::resizeGL(int w, int h)
+{
+    update();
+
+}
+
+void HelloOpenGL::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+        case Qt::Key_P:
+             key = "Key_P";
+             break;
+        case Qt::Key_O:
+             key = "Key_O";
+             break;
+    }
+    update();
+
+}
